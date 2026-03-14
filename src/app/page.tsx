46b877937b2a7352883ceb9845ef9dd43ce88f72
ThinThaZin <thinyanant19500@gmail.com>
2026-03-14 13:45:26 +0700

    add new demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -241,6 +241,15 @@ const Projects = () => {
             if (repo.name === 'movie-lists-and-reviews' && !repo.homepage) {
               return { ...repo, homepage: 'https://movie-lists-and-reviews-qxrb.vercel.app' };
             }
+            if (repo.name === 'FoodFusion' && !repo.homepage) {
+              return { ...repo, homepage: 'https://foodfusion-production.up.railway.app' };
+            }
+            if ((repo.name === 'EduVibe-OnlineEducationPlaform-Frontend' || repo.name === 'EduVibe-OnlineEducationPlaform') && !repo.homepage) {
+              return { ...repo, homepage: 'https://eduvibe-onlineeducationplaform-frontend-production.up.railway.app/' };
+            }
+            if (repo.name === 'EduVibe-OnlineEducationPlaform-Backend' && !repo.homepage) {
+              return { ...repo, homepage: 'https://eduvibe-onlineeducationplaform-backend-production.up.railway.app/' };
+            }
             return repo;
           });
           setProjects(updatedData);
